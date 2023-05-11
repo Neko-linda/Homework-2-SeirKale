17af935a0cebe33e26c965eb03042a2fe68ce851
User Name <emelinda.gutierrez04@gmail.com>
2023-05-10 22:53:25 -0400

    finished loop practice

---
+++ Loops.js
@@ -0,0 +1,31 @@
+//1.Create a loop that logs the numbers from 0-99 (ascending)
+
+for (let i = 0; i < 99; i++) {
+    console.log(i)
+}
+
+//2.Create a loop that logs the EVEN numbers from 0-98 (ascending)
+
+for (let i = 2; i < 98; i+= 2) {
+console.log(i)
+}
+
+//3.Create a loop that logs the numbers from 49-72 (ascending)
+
+for (let i = 49; i < 72; i++) {
+    console.log(i)
+}
+
+//4.Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+
+for (let i = 3; i < 90; i++) {
+    if (i % 3 === 0) {
+        console.log(i)
+    }
+}
+
+//5. Create a loop that logs the numbers from 99-0 (descending)
+
+for (let i = 99; i > 0; i--) {
+    console.log(i)
+}
